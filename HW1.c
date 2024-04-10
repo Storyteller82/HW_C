edc12dd0d01e3b834547acea1ec0e34d3263a22e
Storyteller82 <166534721+Storyteller82@users.noreply.github.com>
2024-04-10 23:30:50 +0800

    Add files via upload

---
+++ HW1.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf ("Let's\n");
+    printf ("Let's'");
+    return 0;
+}
+
---
+++ HW2.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf ("Let's\n");
+    printf ("\t go\n");
+    printf ("\t\t to walk");
+    return 0;
+}
+
